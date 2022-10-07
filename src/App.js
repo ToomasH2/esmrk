fe09af31079a63f8c4dd98d924c7e589c85bf6b2
ToomasH2 <herodes.toomas@gmail.com>
2022-10-07 21:49:45 +0300

    page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import PeamineLeht from "./components/peamineLeht";
 import TereTulemast from "./components/tereTulemast";
 import BootstrapTest from "./components/bootstrapTest";
+import Tere from "./components/tere";
+
 import Isiklik from "./components/isiklik";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Footer } from "./elements/footer";
@@ -13,6 +15,7 @@ function App() {
       <Router>
         <div className="main-container">
           <Routes>
+            <Route path="/" element={<Tere />} />
             <Route path="/login" element={<TereTulemast />} />
             <Route path="/peamineLeht" element={<PeamineLeht />} />
             <Route path="/bootstrapTest" element={<BootstrapTest />} />
---
+++ src/components/tere.js
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from "react";
+
+export default function Tere() {
+  return (
+    <div>
+      <h1>tere</h1>
+    </div>
+  );
+}
