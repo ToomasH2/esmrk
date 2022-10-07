b4ed73a015dc4934482f270772a766a345d9f011
ToomasH2 <herodes.toomas@gmail.com>
2022-10-07 21:58:24 +0300

    pag

--- src/components/tere.js
+++ src/components/tere.js
@@ -1,6 +1,9 @@
-import React, { useEffect, useState } from "react";
-
+import React from "react";
+import { useEffect } from "react";
 export default function Tere() {
+  useEffect(() => {
+    document.title = "My Page Title";
+  });
   return (
     <div>
       <h1>tere</h1>
