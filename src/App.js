edb5a57485e57463336e51cdeeb399f9817cc29f
ToomasH2 <herodes.toomas@gmail.com>
2022-10-08 19:22:04 +0300

    yles

--- src/App.js
+++ src/App.js
@@ -5,23 +5,21 @@ import BootstrapTest from "./components/bootstrapTest";
 import Tere from "./components/tere";
 
 import Isiklik from "./components/isiklik";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { Footer } from "./elements/footer";
 
 function App() {
-  const logiValja = () => {};
   return (
     <div>
       <Router>
-        <div className="main-container">
-          <Routes>
-            <Route path="/" element={<Tere />} />
-            <Route path="/login" element={<TereTulemast />} />
-            <Route path="/peamineLeht" element={<PeamineLeht />} />
-            <Route path="/bootstrapTest" element={<BootstrapTest />} />
-            <Route path="/isiklik" element={<Isiklik />} />
-          </Routes>
-        </div>
+        <Routes>
+          <Route exact path="/" element={<Tere />} />
+          <Route path="/login" element={<TereTulemast />} />
+          <Route path="/peamineLeht" element={<PeamineLeht />} />
+          <Route path="/bootstrapTest" element={<BootstrapTest />} />
+          <Route path="/isiklik" element={<Isiklik />} />
+        </Routes>
       </Router>
       <h1>asdasd</h1>
       <Footer />
